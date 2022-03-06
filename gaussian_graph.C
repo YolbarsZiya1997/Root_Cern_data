60c7920047859d85dd62adb3aa4e077c6f1558fe
Ziya <ziyabay135@gmail.com>
2022-03-06 22:43:50 +0300

    Few

--- gaussian_graph.C
+++ gaussian_graph.C
@@ -2,5 +2,7 @@
 
 {
     g = new TGraph();
-    for (int i=0; i<1000; i++) g->SetPoint(i, gRandom->Gaus(5,1), gRandom->Gaus(0,2))
+    for (int i=0; i<100; i++) g->SetPoint(i, gRandom->Gaus(5,1), gRandom->Gaus(0,2));
+    g->SetMarkerStyle(22);
+    g->Draw("ACP")
 }
